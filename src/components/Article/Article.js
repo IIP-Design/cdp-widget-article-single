cb65f3f71a5fddeb71c484da289299b1234a466f
mrewers <marekrewers@gmail.com>
2018-07-31 17:07:08 -0400

    Refactor article content and branding components

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,55 +1,21 @@
 import React, { Component } from 'react';
-import { getItemRequest } from '../../utils/api';
-import { normalizeItem } from '../../utils/parser';
 
 import './Article.css';
 
 class ArticleItem extends Component {
-  constructor() {
-    super();
-    this.props = { id: 531295 }
-    console.log(this.props);
-    const { item } = {};
-    this.state = {
-      item,
-    };
-  }
-  
-
-  getArticle(){
-    const { item } = this.state;
-    const site = "share.dev.america.gov";
-    getItemRequest(site, 531295)
-      .then( response => this.onFetchResult( item ) )
-      .then(console.log(response));
-    
-    // this.setState( {
-    //   article: article
-    // } );  
-    // console.log(this.state)
-  }
-
-  onFetchResult = ( response ) => {
-    if ( response && response.hits.total > 0 ) {
-      const item = normalizeItem( response.hits.hits[0] );
-      this.setState( {
-        item
-      } );
-    }
-  }
 
   render(){
-    const { article } = this.state;
+    const data = this.props.data;
     return (
       <div className="cdp-article-single">
-        {/* <div className="cdp-article-single-media">
-          <img className="cdp-article-single-img" src={ article.thumbnail.large.url } alt="" />
+        <div className="cdp-article-single-media">
+          <img className="cdp-article-single-img" src={ data.thumbnail.large.url } alt="" />
         </div>
         <div className="cdp-article-single-content">
-          <h1 className="cdp-article-single-title">{ article.title }</h1>
-          <div className="cdp-article-single-meta">{ article.author.name + ' - ' + article.published }</div>
-          <div className="cdp-article-single-content" dangerouslySetInnerHTML={{__html: article.content }}></div>
-        </div> */}
+          <h1 className="cdp-article-single-title">{ data.title }</h1>
+          <div className="cdp-article-single-meta">{ data.author.name + ' - ' + data.published }</div>
+          <div className="cdp-article-single-content" dangerouslySetInnerHTML={{__html: data.content }}></div>
+        </div>
       </div>
     );
   }
