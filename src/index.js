101f685631194058f07ac266f92babb80b1fd693
mrewers <marekrewers@gmail.com>
2018-07-31 17:04:48 -0400

    Set up config queries in indices

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import ArticleSingle from './articleSingle';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<ArticleSingle />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css';
+
+export const widgets =  {
+  Article: {
+    new: function( config ) {
+      return {
+        render: () => {
+          if ( !config || !config.selector ) {
+            console.log('Please add a valid DOM node to add component to');
+            return;
+          }
+
+          // let defaultConfig = getDefaultConfig();
+
+          ReactDOM.render (
+            <ArticleSingle 
+              selector={ config.selector }
+              query={ config.query }
+              sites={ config.sites }
+              from={ config.from }
+              size={ config.size }
+              ids={ config.ids }
+              langs={ config.langs }
+              types={ config.types }
+              tags={ config.tags }
+              categories={ config.categories }
+              meta={ config.meta }
+              // ui={ getUIConfig(config) }
+            />, document.querySelector( config.selector )
+          );
+        }
+      }
+    }
+  }
+}
