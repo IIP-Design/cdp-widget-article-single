3283539add7ab995cafa5c84c2a3dfd8fad33479
mrewers <marekrewers@gmail.com>
2018-07-31 17:06:05 -0400

    Replace App with articleSingle

--- src/articleSingle.js
+++ src/articleSingle.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import Branding from './components/Branding/Branding';
-import ArticleItem from './components/Article/Article';
+import ArticleContainer from './containers/ArticleCointainer';
 
 import './articleSingle.css';
 
 class ArticleSingle extends Component {
   render() {
     return (
-      <div className="App">
-        <ArticleItem />
-        <Branding />
+      <div className="cdp-single-article">
+        <ArticleContainer config={ this.props } />
       </div>
     );
   }
