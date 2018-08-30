9f6afff129439d815b590812185f59f9a271999d
mrewers <mrewers@users.noreply.github.com>
2018-08-30 13:16:37 -0400

    Add cdp module loader

---
+++ src/cdp-module-loader.js
@@ -0,0 +1,29 @@
+const loadModule = () => {
+  const existingScript = document.getElementById( 'cdpArticle' );
+  const { id, site } = existingScript.dataset;
+
+  if ( existingScript ) {
+    const css = document.createElement( 'link' );
+    css.setAttribute( 'rel', 'stylesheet' );
+    css.setAttribute( 'type', 'text/css' );
+    css.setAttribute( 'href', `${process.env.PUBLIC_URL}/cpd-module-article-single.min.css` );
+    document.head.appendChild( css );
+
+    const script = document.createElement( 'script' );
+    script.src = `${process.env.PUBLIC_URL}/cpd-module-article-single.min.js`;
+    script.defer = true;
+    document.body.appendChild( script );
+
+    script.onload = () => {
+      if ( CDP ) { // eslint-disable-line no-undef
+        CDP.widgets.Article.new( { // eslint-disable-line no-undef
+          selector: '#cdp-article-embed',
+          sites: site,
+          ids: id
+        } ).render();
+      }
+    };
+  }
+};
+
+loadModule();
