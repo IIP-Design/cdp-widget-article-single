fbb1cf31b7a6251cba84f7454646864a4467d8bf
mrewers <mrewers@users.noreply.github.com>
2018-09-25 21:47:27 -0400

    Adjust styling on featured image caption

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -7,10 +7,12 @@ import './Article.css';
 const ArticleItem = ( { data, lang } ) => (
   <article className="cdp-article-single" style={ { direction: `${lang.textDirection}` } }>
     { data.thumbnail && (
-      <figure className="cdp-article-single-media">
-        <img className="cdp-article-single-img aligncenter" src={ data.thumbnail } alt={ data.thumbnailMeta.alt } />
-        <figcaption>{ data.thumbnailMeta.caption }</figcaption>
-      </figure>
+      <div className="cdp-article-single-media">
+        <figure className="aligncenter">
+          <img className="cdp-article-single-feat-img" src={ data.thumbnail } alt={ data.thumbnailMeta.alt } />
+          <figcaption className="cdp-article-single-feat-cap">{ data.thumbnailMeta.caption }</figcaption>
+        </figure>
+      </div>
     ) }
     <div className="cdp-article-single-content">
       <h1 className="cdp-article-single-title">{ data.title }</h1>
