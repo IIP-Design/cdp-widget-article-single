89814a8a5641c9cc05195686974463436ff70ef5
mrewers <mrewers@users.noreply.github.com>
2020-03-19 10:27:54 -0400

    Switch linting to GPA/LAB config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,12 @@
 module.exports = {
-  extends: ['airbnb'],
+  extends: ['@gpa-lab/eslint-config', '@gpa-lab/eslint-config/react'],
   rules: {
-    'max-len': [1, 120, 2, { ignoreComments: true }],
-    'react/forbid-prop-types': 0,
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
+        aspects: ['noHref', 'invalidHref', 'preferButton'],
         components: ['Link'],
-        specialLink: ['to', 'hrefLeft', 'hrefRight'],
-        aspects: ['noHref', 'invalidHref', 'preferButton']
-      }
-    ],
-    'react/require-default-props': 0,
-    'react/jsx-curly-spacing': [2, { when: 'always', children: true }],
-    'space-in-parens': ['warn', 'always'],
-    'array-bracket-newline': ['warn', { multiline: true, minItems: 3 }],
-    'comma-dangle': ['warn', 'never'],
-    'no-underscore-dangle': 0,
-    'import/first': 0,
-    'import/no-unresolved': [2, { ignore: ['.css$'] }],
-    'no-console': 0,
-    'no-param-reassign': 0,
-    'no-unused-vars': ['warn', { vars: 'local', args: 'none' }],
-    'import/prefer-default-export': 0,
-    'consistent-return': 0,
-    'import/no-extraneous-dependencies': [
-      'warn',
-      {
-        devDependencies: true,
-        optionalDependencies: true,
-        peerDependencies: true
+        specialLink: ['to', 'hrefLeft', 'hrefRight']
       }
     ]
   },
@@ -40,6 +16,7 @@ module.exports = {
     es6: true,
     node: true
   },
+  ignorePatterns: ['build/', 'docs/_site/', 'node_modules/'],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
